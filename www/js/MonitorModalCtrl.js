c76e9d5c78e0cd48f938218dc97fb67e7bcac51d
PliablePixels <pliablepixels@gmail.com>
2015-06-04 11:31:09 -0400

    Created new screen for developer options, removed simulation mode (its practically useless)

---
+++ www/js/MonitorModalCtrl.js
@@ -0,0 +1,11 @@
+/* jshint -W041 */
+/* jslint browser: true*/
+/* global cordova,StatusBar,angular,console */
+
+// controller for Modal Monitor View
+
+angular.module('zmApp.controllers').controller('zmApp.MonitorModalCtrl', ['$ionicPopup', '$scope', 'ZMDataModel',  '$ionicSideMenuDelegate', '$ionicLoading', '$ionicModal', '$state', '$http',  function ($ionicPopup,$scope, ZMDataModel,  $ionicSideMenuDelegate, $ionicLoading, $ionicModal, $state, $http, $rootScope) {
+
+
+
+}]);
