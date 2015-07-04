cb91226a89d84b39c47b15fc88ef0b56ac7ad5fd
PliablePixels <pliablepixels@gmail.com>
2015-07-04 17:28:11 -0400

    fixed screen bouncing on input

--- www/js/app.js
+++ www/js/app.js
@@ -296,6 +296,9 @@ angular.module('zmApp', [
 
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            // solves screen bouncing on form input
+            // since I am using JS Scroll
+            cordova.plugins.Keyboard.disableScroll(true);
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
