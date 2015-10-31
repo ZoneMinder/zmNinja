3953d11735f64949422fd1531a1ac028643d54b6
Arjun Roychowdhury <pliablepixels@gmail.com>
2015-10-31 17:02:44 -0400

    edits

--- hooks/before_prepare/01_pp_hacks.sh
+++ hooks/before_prepare/01_pp_hacks.sh
@@ -7,22 +7,40 @@ echo ----------------------------------------------------
 echo Pliable Pixels build pre-preprocessing
 echo ----------------------------------------------------
 echo Curr Dir: `pwd`
-echo "Copying Modified GCMIntentService for custom sound"
-exe cp www/external/GCMIntentService.java plugins/phonegap-plugin-push/src/android/com/adobe/phonegap/push/
-exe cp www/external/GCMIntentService.java platforms/android/src/com/adobe/phonegap/push
+
+if [ -d "plugins/phonegap-plugin-push/src/android/com/adobe/phonegap/push/" ]; then
+	echo "Copying Modified GCMIntentService for custom sound"
+	exe cp www/external/GCMIntentService.java plugins/phonegap-plugin-push/src/android/com/adobe/phonegap/push/
+	exe cp www/external/GCMIntentService.java platforms/android/src/com/adobe/phonegap/push
+else
+	echo "Directory plugins/phonegap-plugin-push/src/android/com/adobe/phonegap/push/ does not exist, skipping..."
+fi
 
 echo "Copying custom sound"
 echo "---------------------"
-exe mkdir -p platforms/android/res/raw/
-exe cp www/sounds/blop.mp3 platforms/android/res/raw/
-exe cp www/sounds/blop.caf platforms/ios/zmNinja/Resources
+
+if [ -d "platforms/android" ]; then
+	exe mkdir -p platforms/android/res/raw/
+	exe cp www/sounds/blop.mp3 platforms/android/res/raw/
+	exe cp www/sounds/blop.caf platforms/ios/zmNinja/Resources
+else
+	echo "Directory platforms/androdi does not exist, skipping..."
+fi
 
 echo "Copying plist hack for iOS for non SSL connections"
 echo "--------------------------------------------------"
-exe cp www/external/zmNinja-Info.plist.IOS9nonSSLPatch platforms/ios/zmNinja/zmNinja-Info.plist
+if [ -d "platforms/ios/zmNinja" ]; then
+	exe cp www/external/zmNinja-Info.plist.IOS9nonSSLPatch platforms/ios/zmNinja/zmNinja-Info.plist
+else
+	echo "Directory platforms/ios/zmNinja does not exist, skipping..."
+fi
 
 echo "Copying Android notification icons to resource dir"
 echo "--------------------------------------------------"
-exe cp -R www/external/android-notification-icons/ platforms/android/res/
+if [ -d "platforms/android/res/" ]; then
+	exe cp -R www/external/android-notification-icons/ platforms/android/res/
+else
+	echo "Directory platforms/android/res/ does not exist, skipping..."
+fi
 
 
