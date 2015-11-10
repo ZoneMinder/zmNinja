0a13732f18898ae2cee671336c47f1bf41ff31dd
Arjun Roychowdhury <pliablepixels@gmail.com>
2015-11-09 19:16:25 -0500

    #70
    Former-commit-id: e3faea321ed42c3eb3a6c3d3a2a35af86025c7a5

--- www/js/TimelineCtrl.js
+++ www/js/TimelineCtrl.js
@@ -126,9 +126,9 @@ angular.module('zmApp.controllers').controller('zmApp.TimelineCtrl', ['$ionicPla
         console.log ("**** defaultVideo is " + evideo);
         
         $scope.playbackURL = $scope.loginData.url;
-            if ($rootScope.platformOS == "desktop") {
+           /* if ($rootScope.platformOS == "desktop") {
                 $scope.playbackURL = zm.desktopUrl;
-            }
+            }*/
 
         
         $scope.defaultVideo = evideo;
