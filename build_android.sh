fc24bc0287e45ef26b403376e6ddecd7f5a76194
pliablepixels <pliablepixels@gmail.com>
2016-02-02 16:17:47 -0500

    conditional compilation

--- build_android.sh
+++ build_android.sh
@@ -8,55 +8,72 @@ if [ ! -f "$NINJAKEYSTORE" ]; then
 fi
 
 
-echo "Building Release mode for Xwalk android..."
-echo "--------------------------------------------"
 rm -fr platforms/android/build/outputs/*
-echo "Adding crosswalk..."
-ionic browser add crosswalk@15.44.384.9
-cp "$NINJAKEYSTORE" platforms/android
-ionic build android --release
-
 rm -f release_files/*
-cp platforms/android/build/outputs/apk/android-x86-release-unsigned.apk release_files/
-cp platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk release_files/
-echo "Copied files to release_files"
+
+BUILD_MODE="all"
+if [ "$1" = "1" ]; then
+	BUILD_MODE="xwalk"
+	echo "only building crosswalk"
+fi
+
+if [ "$1" = "2" ]; then
+	BUILD_MODE="native"
+	echo "only building native view (5+)"
+fi
+
+if [ "$BUILD_MODE" = "xwalk" ] || [ "$BUILD_MODE" = "all" ]; then
+
+	echo "Building Release mode for Xwalk android..."
+	echo "--------------------------------------------"
+	echo "Adding crosswalk..."
+	ionic browser add crosswalk@15.44.384.9
+	cp "$NINJAKEYSTORE" platforms/android
+	ionic build android --release
+
+	cp platforms/android/build/outputs/apk/android-x86-release-unsigned.apk release_files/
+	cp platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk release_files/
+	echo "Copied files to release_files"
 
 
-#read -p "Press any key to jarsign... " -n1 -s
-cd release_files/
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-armv7-release-unsigned.apk zmNinja
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-x86-release-unsigned.apk zmNinja
+	#read -p "Press any key to jarsign... " -n1 -s
+	cd release_files/
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-armv7-release-unsigned.apk zmNinja
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-x86-release-unsigned.apk zmNinja
 
-#read -p "Press any key to zipalign... " -n1 -s
+	#read -p "Press any key to zipalign... " -n1 -s
 
-~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-x86-release-unsigned.apk zmNinja-x86-pre5.apk
-~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-armv7-release-unsigned.apk zmNinja-arm-pre5.apk
-rm -f android-x86-release-unsigned.apk android-armv7-release-unsigned.apk
+	~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-x86-release-unsigned.apk zmNinja-x86-pre5.apk
+	~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-armv7-release-unsigned.apk zmNinja-arm-pre5.apk
+	rm -f android-x86-release-unsigned.apk android-armv7-release-unsigned.apk
 
-cd ..
+	cd ..
+fi
+
+if [ "$BUILD_MODE" = "native" ] || [ "$BUILD_MODE" = "all" ]; then
+
+	echo "Building Release mode for android 5+..."
+	echo "--------------------------------------------"
+	rm -fr platforms/android/build/outputs/*
+	echo "Adding default browser..."
+	ionic browser revert android
+	cp "$NINJAKEYSTORE" platforms/android
+	ionic build android --release -- --minSdkVersion 21
+
+	cp platforms/android/build/outputs/apk/android-release-unsigned.apk release_files/
+	echo "Copied files to release_files"
 
 
-echo "Building Release mode for android 5+..."
-echo "--------------------------------------------"
-rm -fr platforms/android/build/outputs/*
-echo "Adding default browser..."
-ionic browser revert android
-cp "$NINJAKEYSTORE" platforms/android
-ionic build android --release -- --minSdkVersion 21
+	#read -p "Press any key to jarsign... " -n1 -s
+	cd release_files/
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-release-unsigned.apk zmNinja
 
-cp platforms/android/build/outputs/apk/android-release-unsigned.apk release_files/
-echo "Copied files to release_files"
+	#read -p "Press any key to zipalign... " -n1 -s
 
+	~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-release-unsigned.apk zmNinja.apk
+	rm -f android-release-unsigned.apk 
 
-#read -p "Press any key to jarsign... " -n1 -s
-cd release_files/
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-release-unsigned.apk zmNinja
-
-#read -p "Press any key to zipalign... " -n1 -s
-
-~/Library/Android/sdk/build-tools/22.0.1/zipalign -v 4 android-release-unsigned.apk zmNinja.apk
-rm -f android-release-unsigned.apk 
-
-cd ..
+	cd ..
+fi
 
 
