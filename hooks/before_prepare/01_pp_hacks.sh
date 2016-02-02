cdd79ac61beb1db8c7135252fb57e7e1a4e78678
pliablepixels <pliablepixels@gmail.com>
2016-02-02 11:40:05 -0500

    #157 - SSL certificate install should no longer be needed
    Former-commit-id: 32b58f287cd4088375f2296c405ce121b7459570

--- hooks/before_prepare/01_pp_hacks.sh
+++ hooks/before_prepare/01_pp_hacks.sh
@@ -24,7 +24,7 @@ if [ -d "platforms/android" ]; then
 	exe cp www/sounds/blop.mp3 platforms/android/res/raw/
 	exe cp www/sounds/blop.caf platforms/ios/zmNinja/Resources
 else
-	echo "Directory platforms/androdi does not exist, skipping..."
+	echo "Directory platforms/android does not exist, skipping..."
 fi
 
 echo "Copying plist hack for iOS for non SSL connections"
@@ -42,5 +42,21 @@ if [ -d "platforms/android/res/" ]; then
 else
 	echo "Directory platforms/android/res/ does not exist, skipping..."
 fi
+
+echo "Fixing insecure SSL permission problem"
+echo "--------------------------------------------------"
+if [ -d "platforms/android/CordovaLib/src/org/apache/cordova/engine" ]; then
+	exe cp www/external/SystemWebViewClient.java platforms/android/CordovaLib/src/org/apache/cordova/engine
+else
+	echo "Directory platforms/android/CordovaLib/src/org/apache/cordova/engine does not exist, skipping..."
+fi
+if [ -d "platforms/ios/zmNinja/Classes" ]; then
+	exe cp www/external/AppDelegate.m platforms/ios/zmNinja/Classes/
+else
+	echo "Directory platforms/ios/zmNinja/Classes does not exist, skipping..."
+fi
+
+
+
 
 
