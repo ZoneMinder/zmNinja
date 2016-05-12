aea8f55e17d7f1e3cc60b99704b4798341a8aecd
pliablepixels <pliablepixels@gmail.com>
2016-05-12 15:45:27 -0400

    #248 - green/orange/red dots

--- www/js/DataModel.js
+++ www/js/DataModel.js
@@ -1022,6 +1022,7 @@ angular.module('zmApp.controllers')
                             
                             for ( var i = 0; i< monitors.length; i++)
                             {
+                               
                                 monitors[i].Monitor.listDisplay='show';
                                 monitors[i].Monitor.isAlarmed = false;
                                 monitors[i].Monitor.connKey = (Math.floor((Math.random() * 999999) + 1)).toString();
