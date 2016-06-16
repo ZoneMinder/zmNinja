5c2fa61cb8cfc752e4b67c21172d830956579b1a
pliablepixels <pliablepixels@gmail.com>
2016-06-16 14:03:59 -0400

    cleanup
    Former-commit-id: 0ecca0dcb5a58b2431096004d85f452d69fa4a14

--- maketag.sh
+++ maketag.sh
@@ -2,7 +2,6 @@
 read -p "Please generate CHANGELOG and commit it BEFORE you tag. Press a key when ready..."
 orig=$1
 VER="${orig/v//}"
-echo $VER
-exit
+echo "Creating tag:$VER"
 git tag -fa v$1 -m'v$1'
 git push -f --tags
