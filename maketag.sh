824560b9966a14e121b011157a49a06bcb2ec754
pliablepixels <pliablepixels@gmail.com>
2016-06-16 14:02:53 -0400

    remove v
    Former-commit-id: 71f58689a3e3ae51f77a788d4471ad40784eb022

--- maketag.sh
+++ maketag.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 read -p "Please generate CHANGELOG and commit it BEFORE you tag. Press a key when ready..."
+orig=$1
+VER="${orig/v//}"
+echo $VER
+exit
 git tag -fa v$1 -m'v$1'
 git push -f --tags
