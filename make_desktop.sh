56d3c1227b9ae37c8ec415a68146cba615baa2fa
Pliable Pixels <pliablepixels@gmail.com>
2016-07-12 09:27:55 -0400

    fixed app version replacement
    Former-commit-id: 8c8e7d803d7ed18d3debdcdbe628475d831d8903

--- make_desktop.sh
+++ make_desktop.sh
@@ -25,7 +25,9 @@ if [ -d "$i" ]; then
 	exe cp electron_js/* $i/app
 	exe cp www/ZMNINJA-LICENSE-DESKTOP-CLIENT.txt ../$DIRNAME
 	exe cd $i
-	cat app/js/DataModel.js | sed "s/var zmAppVersion=\"unknown\"/var zmAppVersion=\"$APPVER\"/" > app/js/DataModel.js.tmp
+	cat app/js/DataModel.js | sed "s/var zmAppVersion[ ]*=[ ]*\"unknown\"/var zmAppVersion=\"$APPVER\"/" > app/js/DataModel.js.tmp
+  cat app/js/DataModel.js.tmp >~/foo
+  exit
 	exe rm -fr app/js/DataModel.js
 	exe mv app/js/DataModel.js.tmp app/js/DataModel.js
 	
