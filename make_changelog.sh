ccfa4b4a29aaf5898bc4abdb7ea6eb9ca84cdd7d
Pliable Pixels <pliablepixels@gmail.com>
2017-11-06 05:53:35 -0500

    nits

--- make_changelog.sh
+++ make_changelog.sh
@@ -1,1 +1,7 @@
-github_changelog_generator -u pliablepixels -p zmNinja
+if [ -z "$1" ]; then
+    echo "Please enter a release version"
+    exit
+fi
+
+read -p "Future release is $1. Please press any key to confirm..."
+github_changelog_generator -u pliablepixels -p zmNinja  --future-release $1
