c2fc0c93d9bdce376726c9caa6694c772c03dff4
Pliable Pixels <pliablepixels@gmail.com>
2018-04-27 16:21:44 -0400

    remind once a month

--- www/js/app.js
+++ www/js/app.js
@@ -107,7 +107,7 @@ angular.module('zmApp', [
     thumbWidth:200,
     alarmStatusTime: 10000,
     eventServerErrorDelay:5000, // time to wait till I report initial connect errors
-    zmVersionCheckNag: 15 * 24 // in hrs 
+    zmVersionCheckNag: 30 * 24 // in hrs 
 
   })
 
