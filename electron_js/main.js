d8567a1819d5f557d6e10f0df55ff010903b414a
Pliable Pixels <pliablepixels@gmail.com>
2018-05-01 15:07:07 -0400

    test from vscode for sign

--- electron_js/main.js
+++ electron_js/main.js
@@ -19,7 +19,7 @@ if (argv.path) {
 
 
 
-
+//
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
