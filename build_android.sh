345cdd4a38812d807ebc0c8bc75baa632300d9ce
Pliable Pixels <pliablepixels@gmail.com>
2018-05-21 10:13:18 -0700

    modifications to build process to get versionCode right

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+    APPVER=`cat config.xml | grep "widget " | sed 's/.* version=\"\([^\"]*\)\" xmlns.*/\1/'`
+    # multipleApk adds 2 and 4 in Xwalk builds for arm and x86 respectively
+    ver_pre5=${APPVER//.} 
+    ver=${APPVER//.}9
+
 # App signining credentials in this file
 NINJAKEYSTORE=~/Desktop/zmNinja.keystore
 
@@ -28,7 +33,7 @@ fi
 ############ Crosswalk build ####################################
 if [ "$BUILD_MODE" = "xwalk" ] || [ "$BUILD_MODE" = "all" ]; then
 
-    echo "Building Release mode for Xwalk android..."
+    echo "${ver_pre5}: Building Release mode for Xwalk android..."
     echo "--------------------------------------------"
     echo "Removing android and re-adding..."
     cordova platform remove android
@@ -45,14 +50,16 @@ if [ "$BUILD_MODE" = "xwalk" ] || [ "$BUILD_MODE" = "all" ]; then
     cordova plugin remove cordova-plugin-certificates
     cordova plugin add https://github.com/danjarvis/cordova-plugin-crosswalk-certificate
     cp "$NINJAKEYSTORE" platforms/android/
-    cordova build android --release  -- --targetSdkVersion=23
+    echo "Building XWALK ..."
+    #cordova build android --release  -- --targetSdkVersion=23
+    cordova build android --release  -- --gradleArg=-PcdvVersionCode=${ver_pre5}
     
     # copy builds to my release directory
     cp platforms/android/build/outputs/apk/android-x86-release-unsigned.apk release_files/
     cp platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk release_files/
     echo "Copied files to release_files"
 
-    # sign them
+     # sign them
     cd release_files/
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-armv7-release-unsigned.apk zmNinja
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../platforms/android/zmNinja.keystore android-x86-release-unsigned.apk zmNinja
@@ -66,7 +73,7 @@ fi
 ############ Native web view build ###############################
 if [ "$BUILD_MODE" = "native" ] || [ "$BUILD_MODE" = "all" ]; then
 
-    echo "Building Release mode for android 5+..."
+    echo "${ver}: Building Release mode for android 5+..."
     echo "--------------------------------------------"
     APPVER=`cat config.xml | grep "widget " | sed 's/.* version=\"\([^\"]*\)\" xmlns.*/\1/'`
     a=( ${APPVER//./ } )
@@ -89,7 +96,7 @@ if [ "$BUILD_MODE" = "native" ] || [ "$BUILD_MODE" = "all" ]; then
     cp "$NINJAKEYSTORE" platforms/android/
 
     # Make sure native builds are only deployed in devices < Android 5
-    cordova build android --release -- --minSdkVersion=21 --versionCode=${vcode}
+    cordova build android --release -- --minSdkVersion=21 --versionCode=${ver}
 
     # copy build to release folder and sign
     cp platforms/android/build/outputs/apk/android-release-unsigned.apk release_files/
@@ -108,3 +115,9 @@ fi
     echo "*** Phone State Check:"
     ./checkperms.sh release_files/zmNinja.apk | grep PHONE_STATE
 
+    echo "***VERSION CODE CHECKS:"
+    for f in release_files/*; do
+        echo "$f:"
+        `echo $ANDROID_HOME`/build-tools/23.0.1/aapt dump badging $f | grep versionCode
+        `echo $ANDROID_HOME`/build-tools/23.0.1/aapt dump badging $f | grep native-code
+    done
