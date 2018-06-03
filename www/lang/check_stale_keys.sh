2e713ca2af7fb2ce9e496b194d00f51c92d0ee11
Pliable Pixels <pliablepixels@gmail.com>
2018-06-03 09:04:45 -0400

    #640 only add kBookmark

--- www/lang/check_stale_keys.sh
+++ www/lang/check_stale_keys.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 stale=0
-echo
+clearline='\033[K'
+echo "Checking keys..."
 for i in `cat locale-en.json | awk '/^[[:blank:]]*"k/ {print $1}' | sed s/\"//g`
 do
         t=`grep $i ../templates/*.html  ../js/*.js 2>/dev/null | wc -l`
+        echo -en "\r${clearline}${i}..."
         if [ "$t" -eq "0" ]; then
                 ((stale++))
+                echo
                 echo $i occurs $t times
         fi
 done
+echo -e "\r${clearline}All Done!"
+echo 
 echo "========= TOTAL: ${stale} stale keys ========";
 echo
