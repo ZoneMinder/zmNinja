d22b30b48a99e19605a3192e7911d9eec2c3abb2
Pliable Pixels <pliablepixels@gmail.com>
2018-06-30 10:06:45 -0400

    consistency

--- make_changelog.sh
+++ make_changelog.sh
@@ -3,5 +3,7 @@ if [ -z "$1" ]; then
     exit
 fi
 
-read -p "Future release is $1. Please press any key to confirm..."
-github_changelog_generator -u pliablepixels -p zmNinja  --future-release $1
+orig=$1
+VER="${orig/v/}"
+read -p "Future release is v${VER}. Please press any key to confirm..."
+github_changelog_generator -u pliablepixels -p zmNinja  --future-release v${VER}
