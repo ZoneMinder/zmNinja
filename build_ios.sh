6e44ba644037ed32f1661a2235a2840ab78a7071
Pliable Pixels <pliablepixels@gmail.com>
2018-09-23 14:17:04 -0400

    wekwebview fixes

--- build_ios.sh
+++ build_ios.sh
@@ -1,4 +1,9 @@
 echo "*** Using old build system due to XCode 10 issues ** "
 echo "see https://forum.ionicframework.com/t/how-to-build-ionic-cordova-with-xcode-10/142044"
 
+echo "--- readding certificate plugin to make sure... ---"
+ionic cordova plugin remove cordova-plugin-certificates
+ionic cordova plugin add cordova-plugin-certificates
+
+echo "-- building --"
 ionic cordova build ios --release --buildConfig="./build-auto.json"
