3670714e355a19fdb59d99b4d425e2a811eeeb99
Pliable Pixels <pliablepixels@gmail.com>
2018-11-23 19:43:11 -0500

    tag guess

--- make_tag.sh
+++ make_tag.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-read -p "Please generate CHANGELOG and commit it BEFORE you tag. Press a key when ready..."
-orig=$1
-VER="${orig/v/}"
+if [ -z "$1" ]; then
+    echo "Inferring version name from config.xml"
+    APPVER=`cat config.xml | grep "widget " | sed 's/.* version=\"\([^\"]*\)\" xmlns.*/\1/'`
+else
+    APPVER=$1
+fi
+VER="${APPVER/v/}"
 echo "Creating tag:v$VER"
+
+read -p "Please generate CHANGELOG and commit it BEFORE you tag. Press a key when ready..."
 read -p "Press any key to create the tag or Ctrl-C to break..." -n1 
 git tag -fa v$VER -m"v$VER"
 git push -f --tags
