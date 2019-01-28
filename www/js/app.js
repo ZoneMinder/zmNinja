8401b210d83faf56e0795afc4e5b43c68845b0dd
Pliable Pixels <pliablepixels@gmail.com>
2019-01-28 04:18:34 -0500

    fix online/offline detection and wake up hack

--- www/js/app.js
+++ www/js/app.js
@@ -1654,11 +1654,38 @@ angular.module('zmApp', [
       $timeout (function() {
 
       NVR.log ("--------->Setting up network state handlers....");
-      document.addEventListener("offline", onOffline, false);
-      document.addEventListener("online", onOnline, false);
+      window.addEventListener("offline", onOffline, false);
+      window.addEventListener("online", onOnline, false);
+      if ($rootScope.platformOS == 'desktop') {
+        NVR.log ("---> Hacked up waked detection...");
+        detectWake();
+      }
 
       },3000);
 
+
+      // credit: https://blog.alexmaccaw.com/javascript-wake-event
+      function detectWake() {
+        var TIMEOUT = 10000;
+        var iter = 1;
+        var lastTime = (new Date()).getTime();
+
+        setInterval(function() {
+          var currentTime = (new Date()).getTime();
+          if (currentTime > (lastTime + TIMEOUT + 10000)) {
+            // Wake!
+            $rootScope.online = false;
+            NVR.log ("********* YOU WOKE UP!!!!!");
+            onOnline();
+            iter = 1;
+          } 
+          else {
+            //NVR.debug ("alive..."+iter);
+            iter++;
+          }
+          lastTime = currentTime;
+        }, TIMEOUT);
+      }
 
       function onOffline() {
         $timeout(function () {
