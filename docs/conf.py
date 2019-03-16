31ed012f1e51ab1a9ced31cffd61506ff486371a
Pliable Pixels <pliablepixels@gmail.com>
2019-03-16 07:11:54 -0400

    changed paths

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -96,7 +96,7 @@ html_logo = 'img/ninja.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
