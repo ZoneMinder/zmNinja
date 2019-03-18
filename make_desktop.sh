20531fe95e1a326a076e2e6d39dc356cf1573822
Pliable Pixels <pliablepixels@gmail.com>
2019-03-18 16:31:44 -0400

    #789 - script changes

--- make_desktop.sh
+++ make_desktop.sh
@@ -61,7 +61,7 @@ APPVER=`cat config.xml | grep "widget " | sed 's/.* version=\"\([^\"]*\)\" xmlns
 APPVER+="D"
 echo "Application version:$APPVER"
 
-declare -a app_ports=("desktop/zmNinja-mac.app/Contents/Resources" "desktop/zmNinja-linux32bit/resources" "desktop/zmNinja-linux64bit/resources" "desktop/zmNinja-win64bit/resources" "desktop/zmNinja-win32bit/resources"  "desktop/zmNinja-linuxarm/resources")
+declare -a app_ports=("desktop/zmNinja-mac.app/Contents/Resources" "desktop/zmNinja-linux32bit/resources" "desktop/zmNinja-linux64bit/resources" "desktop/zmNinja-win64bit/resources" "desktop/zmNinja-win32bit/resources"  "desktop/zmNinja-linuxarmv7l/resources")
 
 for i in "${app_ports[@]}"
 do
