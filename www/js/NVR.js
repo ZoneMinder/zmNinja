bf5aefcd2069cfe94108e6cac29268769a1a28a9
Pliable Pixels <pliablepixels@gmail.com>
2019-04-11 15:56:22 -0400

    #806 fluid class for image

--- www/js/NVR.js
+++ www/js/NVR.js
@@ -20,7 +20,7 @@ angular.module('zmApp.controllers')
         DO NOT TOUCH zmAppVersion
         It is changed by sync_version.sh
       */
-      var zmAppVersion = "1.3.056";
+      var zmAppVersion = "1.3.057";
       var isBackground = false;
       var justResumed = false;
       var timeSinceResumed = -1;
