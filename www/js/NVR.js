ec20a5be48fb1e8afdc31b8390aa7daab592bfba
Pliable Pixels <pliablepixels@gmail.com>
2019-05-15 16:40:34 -0400

    #817 remove token/auth from logs

--- www/js/NVR.js
+++ www/js/NVR.js
@@ -314,10 +314,15 @@ angular.module('zmApp.controllers')
           if (val !== undefined) {
             var regex1 = /"password":".*?"/g;
             var regex2 = /&pass=.*?(?=["&]|$)/g;
+            var regex3 = /&token=([^&]*)/g;
+            var regex4 = /&auth=([^&]*)/g;
+
 
             //console.log ("VAL IS " + val);
             val = val.replace(regex1, "<password removed>");
             val = val.replace(regex2, "<password removed>");
+            val = val.replace (regex3, "&token=<removed>");
+            val = val.replace (regex4, "&auth=<removed>");
           }
 
           $ionicPlatform.ready(function () {
@@ -492,10 +497,15 @@ angular.module('zmApp.controllers')
           if (val !== undefined) {
             var regex1 = /"password":".*?"/g;
             var regex2 = /&pass=.*?(?=["&]|$)/g;
+            var regex3 = /&token=([^&]*)/g;
+            var regex4 = /&auth=([^&]*)/g;
+
 
             //console.log ("VAL IS " + val);
             val = val.replace(regex1, "<password removed>");
             val = val.replace(regex2, "<password removed>");
+            val = val.replace (regex3, "&token=<removed>");
+            val = val.replace (regex4, "&auth=<removed>");
 
           }
           // make sure password is removed
