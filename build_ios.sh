70e8efd97520d221807be9b5e3f5489a45c56b91
Pliable Pixels <pliablepixels@gmail.com>
2019-07-09 15:19:53 -0400

    fixes for iOS webview plugin using private apis

--- build_ios.sh
+++ build_ios.sh
@@ -1,6 +1,7 @@
 echo "*** Using old build system due to XCode 10 issues ** "
 echo "see https://forum.ionicframework.com/t/how-to-build-ionic-cordova-with-xcode-10/142044"
 
+ionic cordova plugin remove cordova-plugin-ionic-webview 2>/dev/null
 ionic cordova plugin add https://github.com/pliablepixels/cordova-plugin-ionic-webview.git
 cordova plugin remove cordova-plugin-media-pp-fork
 cordova plugin add cordova-plugin-media-pp-fork
