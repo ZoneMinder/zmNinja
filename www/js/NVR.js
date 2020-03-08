4a5abe9b6efd6c9568bf2dfcc08391a154ee6195
Pliable Pixels <pliablepixels@gmail.com>
2020-03-08 08:16:21 -0400

    nits

--- www/js/NVR.js
+++ www/js/NVR.js
@@ -21,7 +21,7 @@ angular.module('zmApp.controllers')
         DO NOT TOUCH zmAppVersion
         It is changed by sync_version.sh
       */
-      var zmAppVersion = "1.3.089";
+      var zmAppVersion = "1.3.090";
      
       var isBackground = false;
       var justResumed = false;
