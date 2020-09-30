3ce00a6fda7538c31c21294f4c6f6b3683662c7b
Pliable Pixels <pliablepixels@users.noreply.github.com>
2020-09-30 10:20:12 -0400

    Revert "Ui fixes"

--- www/js/EventDateTimeFilterCtrl.js
+++ www/js/EventDateTimeFilterCtrl.js
@@ -29,7 +29,6 @@ angular.module('zmApp.controllers')
       $scope.$on('$ionicView.beforeEnter', function () {
         $scope.today = moment().format("YYYY-MM-DD");
         $scope.monitors = NVR.getMonitorsNow();
-        var countChecked = 0;
         if (!$scope.monitors.length) {
           NVR.getMonitors(1)
           .then (function (data) {
@@ -37,8 +36,6 @@ angular.module('zmApp.controllers')
             for (var i=0; i < $scope.monitors.length; i++) {
               if ($scope.monitors[i].Monitor.isChecked == undefined)
                $scope.monitors[i].Monitor.isChecked = true;
-              if ($scope.monitors[i].Monitor.isChecked == true)
-               countChecked++;
             }
           });
         }
@@ -46,14 +43,9 @@ angular.module('zmApp.controllers')
           for (var i=0; i < $scope.monitors.length; i++) {
             if ($scope.monitors[i].Monitor.isChecked == undefined)
                $scope.monitors[i].Monitor.isChecked = true;
-            if ($scope.monitors[i].Monitor.isChecked == true)
-               countChecked++;
           }
         }
-        if (countChecked < $scope.monitors.length)
-          $scope.monitorsExpanded = true;
-        else
-          $scope.monitorsExpanded = false;
+        $scope.monitorsExpanded = false;
       });
 
       $scope.toggleMonitors = function() {
