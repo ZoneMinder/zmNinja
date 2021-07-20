fac12099b641c246ae9299d37c0d6c2645bbbd0b
Pliable Pixels <pliablepixels@gmail.com>
2021-07-20 07:40:23 -0400

    push tags to remote

--- make_tag.sh
+++ make_tag.sh
@@ -12,3 +12,4 @@ read -p "Please generate CHANGELOG and commit it BEFORE you tag. Press a key whe
 read -p "Press any key to create the tag or Ctrl-C to break..." -n1 
 git tag -fa v$VER -m"v$VER"
 git push -f --tags
+git push upstream -f  --tags
