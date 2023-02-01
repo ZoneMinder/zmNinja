db8e6380c5989119b18bfa3ac9ae8a6b6ae24635
autonomous <autonomous666@gmail.com>
2023-02-01 18:24:35 -0500

    updated ssh tunnel event handlers

--- electron_js/preload.js
+++ electron_js/preload.js
@@ -4,7 +4,9 @@ contextBridge.exposeInMainWorld('sshModule', {
     connect_ssh_sync: opts => ipcRenderer.sendSync('connect_ssh_sync', opts),
     generate_keypair: (...args) => ipcRenderer.sendSync("generate_keypair", ...args),
     get_public_key: (...args) => ipcRenderer.sendSync("get_public_key", ...args),
-    ssh_status: (callback) => ipcRenderer.on('ssh_status', callback),
-    ssh_debug: (callback) => ipcRenderer.on('ssh_debug', callback),
-    ssh_error: (callback) => ipcRenderer.on('ssh_error', callback)
+    network_online: (...args) => ipcRenderer.send("network_online", ...args),
+    network_offline: (...args) => ipcRenderer.send("network_offline", ...args),
+    ready: (callback) => ipcRenderer.on('ready', callback),
+    debug: (callback) => ipcRenderer.on('debug', callback),
+    error: (callback) => ipcRenderer.on('error', callback)
 });
